4e317bd6460fce34f96302465cac73cef90ad872
John Higgins <johnmatthiggins@gmail.com>
2024-03-10 13:14:22 -0700

    got content-length header working

--- main.h
+++ main.h
@@ -1,9 +1,17 @@
-#define HTTP_RESPONSE "HTTP/1.1 200 OK\n"\
-                      "Content-Type: application/json\n"\
-                      "Server: Little C Server\n\n"\
-                      "{ message: \"Hello world\"!}\n"
+#define HTTP_RESPONSE "HTTP/1.1 200 OK\r\n"\
+                      "Content-Type: application/json\r\n"\
+                      "Content-Length: 30\r\n"\
+                      "Server: Little C Server\r\n\r\n"\
+                      "{ message: \"Hello world!\" }\r\n\x04"
 
+/* prints error codes for socket() function. */
 void print_socket_err(int err);
+
+/* prints error codes for bind() function. */
 void print_bind_err(int err);
+
+/* prints error codes for listen() function. */
 void print_listen_err(int err);
+
+/* prints error codes for recv() function. */
 void print_recv_err(int err);
